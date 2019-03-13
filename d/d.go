81478cbace3811c286ba027c929b140d89f3c5cd
Daved <daved@codemodus.com>
2019-03-13 16:36:42 -0700

    Add second-level package with go.mod

---
+++ d/d.go
@@ -0,0 +1,6 @@
+package d
+
+// Version returns a representation of the current module version.
+func Version() string {
+	return "v0.1.0"
+}
---
+++ d/go.mod
@@ -0,0 +1,3 @@
+module github.com/daved/multimod/d
+
+go 1.12
